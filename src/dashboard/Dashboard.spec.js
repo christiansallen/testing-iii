b38e2d614702db7cc4849a4d9261168b45dc2720
christiansallen <christiansallen@gmail.com>
2019-03-06 16:02:56 -0700

    Finished MVP, but had one mistake with the controls test to see if button was firing a method.

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
-// Test away
+import React from "react";
+import { render, fireEvent, cleanup } from "react-testing-library";
+import "jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+
+describe("<Dashboard/>", () => {
+  it("sees if controls component renders correctly", () => {
+    const { getByTestId } = render(<Dashboard />);
+    const controls = getByTestId("controls");
+    expect(controls).toBeTruthy();
+  });
+
+  it("sees if display component renders correctly", () => {
+    const { getByTestId } = render(<Dashboard />);
+    const display = getByTestId("display");
+    expect(display).toBeTruthy();
+  });
+});
